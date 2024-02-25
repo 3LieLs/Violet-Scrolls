a1c9e9fd31a44a807e2ddeb71e378fe1d378a45d
3LieLs <3lielfernandesribeiro@gmail.com>
2024-02-25 14:39:35 -0300

    att

--- assets/script/inicio/escolhaNomeGenero.js
+++ assets/script/inicio/escolhaNomeGenero.js
@@ -6,16 +6,14 @@ botaoMasculino.addEventListener('click', botaoMasculinoClick);
 var botaoFeminino = window.document.querySelector('input#botaoFeminino');
 botaoFeminino.addEventListener('click', botaoFemininoClick);
 /*-SELEÇÃO DE GÊNERO ESCOLHA-*/
-function botaoMasculinoClick()
-{
+function botaoMasculinoClick() {
     jogador.genero = 'Homem';
     armaGeral = soco;
     escolhaGenero.style.display = 'none';
     escolhaNome.style.display = 'contents';
 }
 
-function botaoFemininoClick()
-{
+function botaoFemininoClick() {
     jogador.genero = 'Mulher';
     armaGeral = soco;
     escolhaGenero.style.display = 'none';
@@ -27,26 +25,13 @@ function botaoFemininoClick()
 /*-SELEÇÃO DE NOME VARIÁVEIS-*/
 var enviarNomeJogador = window.document.querySelector('input#enviarNomeJogador');
 enviarNomeJogador.addEventListener('click', enviarNomeJogadorClick);
-enviarNomeJogador.addEventListener('mouseover', enviarNomeJogadorHover);
-enviarNomeJogador.addEventListener('mouseout', enviarNomeJogadorOut);
 /*-SELEÇÃO DE NOME ESCOLHA-*/
-function enviarNomeJogadorClick()
-{
+function enviarNomeJogadorClick() {
     jogadorNome = window.document.querySelector('input#nomeJogador');
     jogador.nome = String(jogadorNome.value);
     escolhaNome.style.display = 'none';
     escolhaRaca.style.display = 'contents';
+    jogadorNome.value = ''
 }
-function enviarNomeJogadorHover()
-{
-    enviarNomeJogador.style.cursor = 'pointer';
-    enviarNomeJogador.style.animation = 'botaoAnimacao 2s 0ms infinite running';
-    enviarNomeJogador.style.background = 'gray';
-}
-function enviarNomeJogadorOut()
-{
-    enviarNomeJogador.style.background = 'black';
-    enviarNomeJogador.style.transform = 'scale(1.0)';
-    enviarNomeJogador.style.animation = 'paused';
-}
+
 /*-----*//*-----*//*-----*//*-----*//*-----*/
