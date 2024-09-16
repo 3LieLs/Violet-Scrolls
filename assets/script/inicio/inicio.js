ab58406da3dbed4d7740ccfe9cab6f9d0e29f965
Eliel Fernandes <135179431+3LieLs@users.noreply.github.com>
2024-09-16 11:16:40 -0300

    Update inicio.js

--- assets/script/inicio/inicio.js
+++ assets/script/inicio/inicio.js
@@ -1,27 +1,18 @@
 /*-----*//*-----*//*-----*//*-----*//*-----*/
-/*-INICIO VARIÁVEIS-*/
-var botaoInicioVal = window.document.querySelector('input#botaoInicio');
-botaoInicioVal.addEventListener('click', botaoInicioClick);
-botaoInicioVal.addEventListener('mouseover', botaoInicioHover);
-botaoInicioVal.addEventListener('mouseout', botaoInicioOut);
 /*-INICIO-*/
-function botaoInicioClick()
-{
+botaoInicio.addEventListener('click', () => {
     inicio.style.display = 'none';
     escolhaGenero.style.display = 'contents';
-}
+});
 
-function botaoInicioHover()
-{
-    botaoInicioVal.style.cursor = 'pointer';
-    botaoInicioVal.style.animation = 'botaoAnimacao 2s 0ms infinite running';
-    botaoInicioVal.style.background = 'gray';
-}
-
-function botaoInicioOut()
-{
-    botaoInicioVal.style.background = 'black';
-    botaoInicioVal.style.transform = 'scale(1.0)';
-    botaoInicioVal.style.animation = 'paused';
-}
+botaoInicio.addEventListener('mouseover', () => {
+    botaoInicio.style.cursor = 'pointer';
+    botaoInicio.style.animation = 'botaoAnimacao 2s 0ms infinite running';
+    botaoInicio.style.background = 'gray';
+});
+botaoInicio.addEventListener('mouseout', () => {
+    botaoInicio.style.background = 'black';
+    botaoInicio.style.transform = 'scale(1.0)';
+    botaoInicio.style.animation = 'paused';
+});
 /*-----*//*-----*//*-----*//*-----*//*-----*/
