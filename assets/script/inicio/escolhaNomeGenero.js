211b6eae32cb313c205612a4d32ac97cf13c79ba
Eliel Fernandes <135179431+3LieLs@users.noreply.github.com>
2024-09-16 11:16:58 -0300

    Update escolhaNomeGenero.js

--- assets/script/inicio/escolhaNomeGenero.js
+++ assets/script/inicio/escolhaNomeGenero.js
@@ -1,37 +1,25 @@
 /*-----*//*-----*//*-----*//*-----*//*-----*/
-/*-SELEÇÃO DE GÊNERO VARIÁVEIS-*/
-var botaoMasculino = window.document.querySelector('input#botaoMasculino');
-botaoMasculino.addEventListener('click', botaoMasculinoClick);
-
-var botaoFeminino = window.document.querySelector('input#botaoFeminino');
-botaoFeminino.addEventListener('click', botaoFemininoClick);
 /*-SELEÇÃO DE GÊNERO ESCOLHA-*/
-function botaoMasculinoClick() {
+botaoMasculino.addEventListener('click', () => {
     jogador.genero = 'Homem';
     armaGeral = soco;
     escolhaGenero.style.display = 'none';
     escolhaNome.style.display = 'contents';
-}
+});
 
-function botaoFemininoClick() {
+botaoFeminino.addEventListener('click', () => {
     jogador.genero = 'Mulher';
     armaGeral = soco;
     escolhaGenero.style.display = 'none';
     escolhaNome.style.display = 'contents';
-}
+});
 /*-----*//*-----*//*-----*//*-----*//*-----*/
 
 /*-----*//*-----*//*-----*//*-----*//*-----*/
-/*-SELEÇÃO DE NOME VARIÁVEIS-*/
-var enviarNomeJogador = window.document.querySelector('input#enviarNomeJogador');
-enviarNomeJogador.addEventListener('click', enviarNomeJogadorClick);
 /*-SELEÇÃO DE NOME ESCOLHA-*/
-function enviarNomeJogadorClick() {
-    jogadorNome = window.document.querySelector('input#nomeJogador');
-    jogador.nome = String(jogadorNome.value);
+enviarNomeJogador.addEventListener('click', () => {
+    jogador.nome = String(window.document.querySelector('input#nomeJogador').value);
     escolhaNome.style.display = 'none';
     escolhaRaca.style.display = 'contents';
-    jogadorNome.value = ''
-}
-
+});
 /*-----*//*-----*//*-----*//*-----*//*-----*/
